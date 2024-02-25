5e579b1d9d97925a0ead956871a08b80ca9fd178
faaria <faariahassan2003@gmail.com>
2024-02-25 17:25:58 +0500

    Initial Commit

---
+++ Get hub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Get hub
+//
+//  Created by faria on 2/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Get hub/Get_hubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Get_hubApp.swift
+//  Get hub
+//
+//  Created by faria on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Get_hubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
